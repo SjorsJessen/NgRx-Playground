885680352836ea8873dcb45d7cb33026e8f746ef
Sjors Jessen <sjors.jessen@gmail.com>
2023-02-10 00:01:36 +0100

    added counter component

--- ngrx-playground/src/app/app.module.ts
+++ ngrx-playground/src/app/app.module.ts
@@ -1,24 +1,32 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { StoreModule } from '@ngrx/store';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {StoreModule} from '@ngrx/store';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatCardModule} from "@angular/material/card";
+import {CounterComponent} from './features/counter/components/counter.component';
+import {MatButtonModule} from '@angular/material/button';
+import {counterReducer} from "./features/counter/counter.reducer";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CounterComponent
   ],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        StoreModule.forRoot({}, {}),
-        BrowserAnimationsModule,
-        MatToolbarModule
-    ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    StoreModule.forRoot({count: counterReducer}),
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatCardModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
